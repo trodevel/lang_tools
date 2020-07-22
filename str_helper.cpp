f62e5a7a60dc14b23f9d48bde5610de048b29d61
trodevel <trodevel@gmail.com>
2020-07-23 01:27:45 +0200

    added write()

--- str_helper.cpp
+++ str_helper.cpp
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 5280 $ $Date:: 2016-12-19 #$ $Author: serge $
+// $Revision: 13396 $ $Date:: 2020-07-23 #$ $Author: serge $
 
 #include "str_helper.h"             // self
 
@@ -54,5 +54,19 @@ const std::string & to_string_iso( const lang_e l )
 
     return it->second;
 }
+
+namespace str_helper
+{
+
+// enums
+
+std::ostream & write( std::ostream & os, const lang_e r )
+{
+    os << lang_tools::to_string( r );
+
+    return os;
+}
+
+} // namespace str_helper
 
 NAMESPACE_LANG_TOOLS_END
--- str_helper.h
+++ str_helper.h
@@ -19,9 +19,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 5280 $ $Date:: 2016-12-19 #$ $Author: serge $
+// $Revision: 13396 $ $Date:: 2020-07-23 #$ $Author: serge $
 
 #include <string>
+
+// system includes
+#include <sstream>
 
 #include "language_enum.h"      // lang_e
 
@@ -31,5 +34,23 @@ NAMESPACE_LANG_TOOLS_START
 
 const std::string & to_string( const lang_e l );
 const std::string & to_string_iso( const lang_e l );
+
+namespace str_helper
+{
+
+// enums
+std::ostream & write( std::ostream & os, const lang_e r );
+
+template<class T>
+std::string to_string( const T & l )
+{
+    std::ostringstream os;
+
+    write( os, l );
+
+    return os.str();
+}
+
+} // namespace str_helper
 
 NAMESPACE_LANG_TOOLS_END
