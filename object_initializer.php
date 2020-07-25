70116c92dc606b3dede06ca47569c559c8306107
trodevel <trodevel@gmail.com>
2020-07-26 01:43:18 +0200

    PHP: generated protocol

---
+++ object_initializer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace lang_tools_protocol;
+
+
+// includes
+require_once 'protocol.php';
+
+// objects
+
+// base messages
+
+// messages
+
+// objects (constructors)
+
+// messages (constructors)
+
+# namespace_end lang_tools_protocol
+
+
+?>
